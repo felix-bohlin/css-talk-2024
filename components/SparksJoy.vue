ba9aa1394e7c1cdbb19fbb5cf7068317f2dadb44
Felix Bohlin <felix.bohlin@coor.com>
2024-03-20 11:28:50 +0100

    kondo

--- components/SparksJoy.vue
+++ components/SparksJoy.vue
@@ -3,7 +3,7 @@
     <p>This sparks joy!</p>
     <img
       class="object-contain w-10 drop-shadow-[0_0px_40px_rgba(255,255,2)]"
-      src="../public/kondo.png"
+      src="/kondo.png"
     />
   </div>
 </template>
